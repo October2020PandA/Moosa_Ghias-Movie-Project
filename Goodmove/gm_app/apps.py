2b909dcf7912e862f6f4401775f48efafec79325
Moosa Ghias <moosa.ghias@gmail.com>
2020-10-06 20:32:45 -0700

    adding wireframes

---
+++ Goodmove/gm_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GmAppConfig(AppConfig):
+    name = 'gm_app'
---
+++ Goodmove/gm_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+import re
+import bcrypt
+email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+# Create your models here.
